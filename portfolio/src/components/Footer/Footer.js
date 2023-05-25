4a8af10701e6b47d4ef63512a236d8fe3477711d
Carlos-Ojeda <A01741085@itesm.mx>
2023-05-25 15:03:49 -0600

    Update LinkedIn URL

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             <Divider id="contact" style={{color:"white", borderTopColor:"rgb(131, 131, 131)"}}>Contact Me</Divider>
             
             <Row style={{paddingBottom:"24px"}}justify='space-evenly content'>
-            <Col><a href="https://www.linkedin.com/in/carlos-noe-ojeda-ang"><LinkedinOutlined className='footer-icon' key="linkedin" /></a></Col>
+            <Col><a href="https://www.linkedin.com/in/carlos-ojd/"><LinkedinOutlined className='footer-icon' key="linkedin" /></a></Col>
                 <Col><a href="https://github.com/Carlos-Ojeda"><GithubOutlined className='footer-icon' key="github" /></a></Col>
                 <Col><a href="https://twitter.com/CarlosOGD"><TwitterOutlined className='footer-icon' key="twitter" /></a></Col>
                 <Col><a href="mailto:carlosnoel_ojedaa@icloud.com"><MailOutlined className='footer-icon' key="email" /></a></Col>
